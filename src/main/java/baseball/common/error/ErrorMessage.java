737cff5b26e5386dc44bb08fadbdaa6bd9644489
Hae-gun <newchk610@gmail.com>
2022-09-30 21:59:03 +0900

    feat(domain) : 결과 숫자(정답수) 도메인 정의

---
+++ src/main/java/baseball/common/error/ErrorMessage.java
@@ -0,0 +1,17 @@
+package baseball.common.error;
+
+public enum ErrorMessage {
+
+    INPUT_NUMBER_ERROR("1~9 숫자만 입력가능합니다."),
+    END_GREATER_THAN_START("start 값은 항상 end 값보다 작아야 합니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/baseball/game/ResultNumbers.java
@@ -0,0 +1,34 @@
+package baseball.game;
+
+import baseball.common.error.ErrorMessage;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResultNumbers {
+    private final int LIST_SIZE = 3;
+    private final int MIN_NUM = 1;
+    private final int MAX_NUM = 9;
+    private List<Integer> answers;
+
+    public ResultNumbers(int startInclusive, int endInclusive){
+        inputValidation(startInclusive, endInclusive);
+        answers = Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, LIST_SIZE);
+    }
+
+    public List<Integer> numbers() {
+        return new ArrayList<>(answers);
+    }
+
+    private void inputValidation(int start, int end){
+        if(start < MIN_NUM || end < MIN_NUM || start > MAX_NUM || end > MAX_NUM){
+            throw new IllegalArgumentException(ErrorMessage.INPUT_NUMBER_ERROR.getMessage());
+        }
+        if(start >= end){
+            throw new IllegalArgumentException(ErrorMessage.END_GREATER_THAN_START.getMessage());
+        }
+
+    }
+
+}
