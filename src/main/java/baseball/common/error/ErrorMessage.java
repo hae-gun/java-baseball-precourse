48d87ae01f68d61a37004fa5f277a628ec320466
Hae-gun <newchk610@gmail.com>
2022-09-30 22:21:53 +0900

    test(InputNumbers) : 입력 숫자값 테스트코드 작성

--- src/main/java/baseball/common/error/ErrorMessage.java
+++ src/main/java/baseball/common/error/ErrorMessage.java
@@ -3,7 +3,8 @@ package baseball.common.error;
 public enum ErrorMessage {
 
     INPUT_NUMBER_ERROR("1~9 숫자만 입력가능합니다."),
-    END_GREATER_THAN_START("start 값은 항상 end 값보다 작아야 합니다.");
+    END_GREATER_THAN_START("start 값은 항상 end 값보다 작아야 합니다."),
+    IS_NOT_NUMBER("숫자가 아닌값이 입력되었습니다.");
 
     private final String message;
 
---
+++ src/test/java/baseball/game/InputNumbersTest.java
@@ -0,0 +1,57 @@
+package baseball.game;
+
+import baseball.common.error.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputNumbersTest {
+
+    private String CORRECT_INPUTS = "1 2 3";
+
+    @DisplayName("입력 문자열은 공백을 포함한 숫자만 입력 가능하다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1 2 f", "1.3", "asd"})
+    void inputValidationCheck1(String numStr){
+        assertThatThrownBy(() -> {
+            new InputNumbers(numStr);
+        }).withFailMessage(ErrorMessage.IS_NOT_NUMBER.getMessage());
+
+    }
+
+    @DisplayName("3개의 숫자가 입력되어야 한다.")
+    @Test
+    void inputSizeTest(){
+        InputNumbers inputNumbers = new InputNumbers(CORRECT_INPUTS);
+        assertThat(inputNumbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("숫자 1~9 사이의 값만 입력가능하다.")
+    @Test
+    void inputValidationCheck2(){
+        assertThatThrownBy(() -> {
+            new InputNumbers("1 2 10");
+        }).withFailMessage(ErrorMessage.INPUT_NUMBER_ERROR.getMessage());
+    }
+
+    @DisplayName("서로 다른 숫자를 입력해야 한다.")
+    @Test
+    void inputValidationCheck3(){
+        InputNumbers inputNumbers = new InputNumbers(CORRECT_INPUTS);
+
+        int first = inputNumbers.first(0);
+        int second = inputNumbers.second(1);
+        int third = inputNumbers.third(2);
+
+        assertThat(first).isNotEqualTo(second);
+        assertThat(second).isNotEqualTo(third);
+        assertThat(third).isNotEqualTo(first);
+
+    }
+
+}
